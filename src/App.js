9bea19c0e1b67c4a72ee981407bdbafdac459cf9
Enoch Strok <50689197+enoch-strok@users.noreply.github.com>
2022-04-10 19:32:48 -0500

    fixed errors but loads weird.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Routes ,Route, BrowserRouter } from 'react-router-dom';
-// import { Route, Rout, BrowserRouter  } from "react-router-dom";
+import { render } from "react-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import './App.css';
 
 import About from './pages/about/about';
@@ -13,13 +13,45 @@ import Navbar from './components/navbar/navbar';
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      <Routes>
+      <BrowserRouter>
         <Navbar />
-        <Route path='/' element={About} />
-        <Route path='/quote' element={Quote} />
-        <Route path='/contact' element={Contact} />
-        <Route path='/admin' element={Admin} />
-      </Routes>
+        <div>
+          <Routes>
+            <Route path='/' element={<About />} />
+            <Route path='/quote' element={<Quote />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/admin' element={<Admin />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+
+      {/* <Routes>
+        <Navbar />
+        <Route path='/' element={<About />} />
+        <Route path='/quote' element={<Quote />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes> */}
+            {/* <BrowserRouter >
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+                <About />
+            </Route>
+            <Route path="/quote">
+                <Quote />
+            </Route>
+            <Route path="/contact">
+                <Contact />
+            </Route>
+            <Route path="/admin">
+                <Admin/>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter > */}
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",
+    "./components/**/*.js"
   ],
   theme: {
     extend: {
